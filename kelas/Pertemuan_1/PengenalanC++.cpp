ac72e33602cb6a25e673751e23891672e87ebee9
a-durian <muhammadadrian7333@gmail.com>
2026-03-02 00:20:22 +0800

    Rapihin file github

---
+++ kelas/Pertemuan_1/PengenalanC++.cpp
@@ -0,0 +1,73 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     string nama;
+//     cout < "Masukkan nama kamu: ";
+//     cin > nama;
+//     cout < "Halo, " < nama;
+//     return 0;
+// }
+
+// // \\\ Contoh program untuk menampilkan ukuran tipe data  \\//
+
+// int main() {
+//     cout << "Size of char: " << sizeof(char) << endl;
+//     cout << "Size of int: " << sizeof(int) << endl;
+//     cout << "Size of float: " << sizeof(float) << endl;
+//     cout << "Size of double: " << sizeof(double) << endl;
+//     cout << "Size of long: " << sizeof(long);
+// }
+
+// // \\\ Contoh program untuk menggunakan operator ternary \\//
+// int main() {
+//     bool hujan = false;
+//     string pesan = hujan ? "Bawa payung" : "Tidak perlu payung";
+//     cout << pesan;
+// }
+
+// \\\ Contoh program untuk menggunakan switch-case \\//
+// int main() {
+//     int nilai;
+//     cout << " = Konversi Nilai Akademik =\n";
+//     cout << "Masukkan nilai akhir (0-100): ";
+//     cin >> nilai;
+//     if (nilai < 0 || nilai > 100) {
+//     cout << "Error: Nilai harus 0-100.\n";
+//     return 1;
+//     }
+//     switch (nilai / 10) {
+//     case 10:
+//         cout << "Predikat: A (Sangat Baik)\n";
+//         break;
+//     case 9:
+//         cout << "Predikat: A (Sangat Baik)\n";
+//         break;
+//     case 8:
+//         cout << "Predikat: B (Baik)\n";
+//         break;
+//     case 7:
+//        cout << "Predikat: C (Cukup)\n";
+//       break;
+//     case 6:
+//       cout << "Predikat: D (Kurang)\n";
+//       break;
+//     default:
+//     cout << "Predikat: E (Gagal)\n";
+//     break;
+
+//     }
+//     return 0;
+// }
+
+
+// \\\ Contoh program untuk menggunakan loop do-while \\//
+// int main() {
+//     int i = 0;
+//     do {
+//     cout << i << endl;
+//     i++;
+//     } while (i < 5);
+//     return 0;
+// }
---
+++ test.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+using namespace std;
+
+// ini biar file di github rapi aja 
